d7090f66f2c188e8070533ea09128db07ddedf19
Deepak Saini <98840815+176deepak@users.noreply.github.com>
2022-11-05 23:17:39 +0530

    Update Sort 0 1 2.py

--- Sort 0 1 2.py
+++ Sort 0 1 2.py
@@ -6,17 +6,17 @@ def sort012(arr, n) :
 
 	# write your code here
     # don't return anything 
-    zero = []
-    one = []
-    two = []
+    arr1 = []
+    arr2 = []
+    arr3 = []
     for p in range(len(arr)):
         if arr[p] == 0:
-            zero.append(0)
+            arr1.append(0)
         elif arr[p] == 1:
-            one.append(1)
+            arr2.append(1)
         else:
-            two.append(2)
-    sorted = zero + one + two
+            arr3.append(2)
+    sorted = arr1 + arr2 + arr3
     for i in range(len(sorted)):
         arr[i] = sorted[i]
                
